e0d77ba323988ef9429ea9b49e0ec69e43b3f8b0
Jen Lim <jennolim@gmail.com>
2013-03-31 20:55:07 -0400

    Remove unnecessary code

--- bubble-hilo.rb
+++ bubble-hilo.rb
@@ -4,7 +4,6 @@ myarray = enterednums.split(" ").map {|x| x.to_i}
 solidcount = myarray.count
 puts "Starting Array: #{myarray}"
 
-#while loop starts here? 
 
 arrangedarray = [] 
 while arrangedarray.count != solidcount 
@@ -19,28 +18,3 @@ while arrangedarray.count != solidcount
 	puts "My new array now contains the lowest number and is #{arrangedarray}"
 
 end 
-
-
-
-
-
-# newarray = fixedarray - lowest
-# print newarray  #things are DANDY up till here! Corrected first array 
-# newindex = 0
-
-# #need to map the current minimum to newindex position while adding it 
-# #to newarray 
-
-
-# currentmin = lowest[newarray[(newindex)]]
-# print currentmin
-
-# completenewarray = newarray + (newarray)
-
-
-# #newarraycomplete = lowest + newarray
-# #now the lowest has index0 in myarray. 
-# #keep moving up by adding 1 to index until we reach # of elements in array
-# # puts
-# # print newarraycomplete 
-# # puts "**"
