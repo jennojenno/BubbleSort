4e5470ead67d55017a8894af42605a18063d2bdc
Jen Lim <jennolim@gmail.com>
2013-03-31 20:38:41 -0400

    Add file bubble-hilo.rb for my own version of sorting

---
+++ bubble-hilo.rb
@@ -0,0 +1,47 @@
+myarray = [5,4,6,2,12,3]
+solidcount = myarray.count 
+puts "Starting Array: #{myarray}"
+
+#while loop starts here? 
+
+index = 0
+arrangedarray = [] 
+
+while arrangedarray.count != solidcount
+
+	lowest = myarray[(index)..-1].min #to find the new min after the prev min
+	puts "The next lowest number is #{lowest}."
+	arrangedarray.push(lowest) #adds min # to new array 
+	myarray = myarray - [lowest] #removes min # from old array 
+	puts "Now I have these numbers left to sort: #{myarray}"
+
+	#index += 1 
+	puts "My new array now contains the lowest number and is #{arrangedarray}"
+
+
+end 
+
+
+
+
+
+# newarray = fixedarray - lowest
+# print newarray  #things are DANDY up till here! Corrected first array 
+# newindex = 0
+
+# #need to map the current minimum to newindex position while adding it 
+# #to newarray 
+
+
+# currentmin = lowest[newarray[(newindex)]]
+# print currentmin
+
+# completenewarray = newarray + (newarray)
+
+
+# #newarraycomplete = lowest + newarray
+# #now the lowest has index0 in myarray. 
+# #keep moving up by adding 1 to index until we reach # of elements in array
+# # puts
+# # print newarraycomplete 
+# # puts "**"
